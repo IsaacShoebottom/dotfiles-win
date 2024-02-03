a5992f08d6f2d108a712a237b8f3060c68c7f276
Isaac Shoebottom <ir.shoebottom@gmail.com>
2024-02-02 22:40:00 -0400

    Update powershell shit again

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,8 @@
 # Sync apps with scoop
-&~/Documents/PowerShell/verify-state.ps1
+. ~/Documents/PowerShell/verify-state.ps1
 
 # Shell completion
-&~/Documents/PowerShell/completions.ps1
+. ~/Documents/PowerShell/completions.ps1
 
 # Aliases
-&~/Documents/PowerShell/aliases.ps1
+. ~/Documents/PowerShell/aliases.ps1
--- Documents/PowerShell/aliases.ps1
+++ Documents/PowerShell/aliases.ps1
@@ -3,3 +3,9 @@ if (Test-Path alias:pwd) {
     Remove-Alias -Name pwd
   } 
   Function pwd {(Get-Location).Path}
+
+  # Change to dotfiles directory
+  if (Test-Path alias:dotfolder) {
+    Remove-Alias -Name dotfolder
+  }
+  function dotfolder {Set-Location ~/.local/share/chezmoi}
